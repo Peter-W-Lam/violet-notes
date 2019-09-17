522b07db1850e134aa694c476ba724eed3a31243
Peter Lam <Peter.Lam@tufts.edu>
2019-09-17 08:30:08 -0400

    Updates

---
+++ precache-manifest.8952d83b2ee2b1f2177b9fc04b1cf303.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33deb4d2922a3280d420044d5eba393",
+    "url": "/violet-notes/index.html"
+  },
+  {
+    "revision": "5f111bb65bfa025919dd",
+    "url": "/violet-notes/static/css/main.a4b3de41.chunk.css"
+  },
+  {
+    "revision": "a2e8633d89a2d4165c6c",
+    "url": "/violet-notes/static/js/2.194cc60d.chunk.js"
+  },
+  {
+    "revision": "5f111bb65bfa025919dd",
+    "url": "/violet-notes/static/js/main.1cedfaa2.chunk.js"
+  },
+  {
+    "revision": "6d90b68ac4b189c6032c",
+    "url": "/violet-notes/static/js/runtime~main.672af5d0.js"
+  },
+  {
+    "revision": "ec2d1529975a729af765f2577c202eeb",
+    "url": "/violet-notes/static/media/calendar.ec2d1529.svg"
+  },
+  {
+    "revision": "746547c3217ddb75c60240592a93c044",
+    "url": "/violet-notes/static/media/documents.746547c3.svg"
+  },
+  {
+    "revision": "8424676c6acb3bf9e01120e3271ed3ee",
+    "url": "/violet-notes/static/media/down-arrow.8424676c.svg"
+  },
+  {
+    "revision": "c9d62d2b8c581b97c8999cc911445299",
+    "url": "/violet-notes/static/media/facebook-logo.c9d62d2b.svg"
+  },
+  {
+    "revision": "070d9b997e1467810d101723b74e1a4d",
+    "url": "/violet-notes/static/media/headphones.070d9b99.svg"
+  },
+  {
+    "revision": "aeaf261574a1bcdf78eb5bdd626c0753",
+    "url": "/violet-notes/static/media/home.aeaf2615.svg"
+  },
+  {
+    "revision": "227cee4e5e9abd1b43e098bb78a55b1b",
+    "url": "/violet-notes/static/media/linkedin.227cee4e.svg"
+  },
+  {
+    "revision": "cc671e84b7c9af35878320860c20154a",
+    "url": "/violet-notes/static/media/notes.cc671e84.svg"
+  },
+  {
+    "revision": "464aafec2171c1fc2fbc340cad1a6f01",
+    "url": "/violet-notes/static/media/profile.464aafec.jpg"
+  },
+  {
+    "revision": "a4db6a5268864a299c0f535a1fb317ff",
+    "url": "/violet-notes/static/media/purple-background.a4db6a52.jpg"
+  },
+  {
+    "revision": "cc2bb53bbf13605c6f844658636d91c3",
+    "url": "/violet-notes/static/media/twitter.cc2bb53b.svg"
+  }
+]);
